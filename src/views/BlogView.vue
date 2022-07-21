c2a74e9013dde5863391e5617ee5d91e3e42b868
clintonkenedy <clintonventuracondori@gmail.com>
2022-07-20 21:25:18 -0500

    primer commit realizando primeras vistas y rutas

---
+++ src/views/BlogView.vue
@@ -0,0 +1,13 @@
+<template>
+  blogg
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/PersonaView.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Lista de personas</h1>
+  <p>{{personas}}</p>
+  <table border="1">
+    <tr>
+      <td>NOMBRE</td>
+      <td>EDAD</td>
+      <td>ACCION</td>
+    </tr>
+    <tr v-for="p in personas" :key="p">
+      <td>{{p.nombre}}</td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  data(){
+        return {
+            personas: []
+        }
+  },
+  mounted(){
+       fetch('http://127.0.0.1:8000/api/persona')
+        .then(response => response.json())
+        .then((json) => {
+            console.log(json)
+            this.personas = json
+        }) 
+  }
+}
+</script>
+
+<style>
+
+</style>
